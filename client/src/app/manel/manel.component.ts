9ecb57e7ccb2d63a54181486b86c692c43c0db28
Koumeyl Belkhidar <k.belkhidar@gmail.com>
2021-04-15 18:22:55 +0200

    End of section 6

---
+++ client/src/app/manel/manel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manel',
+  templateUrl: './manel.component.html',
+  styleUrls: ['./manel.component.css']
+})
+export class ManelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
